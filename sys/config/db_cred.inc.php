3be0cfc229767290deb45dca21256de5264ae7eb
Kelleher92 <iankelleher92@gmail.com>
2018-07-16 14:42:51 +0100

    Update db_cred.inc.php

--- sys/config/db_cred.inc.php
+++ sys/config/db_cred.inc.php
@@ -5,21 +5,21 @@
 	/**
 	 * server name
 	 */
-	$DB_ACCESS['DB_HOST'] = 'localhost';
+	$DB_ACCESS['DB_HOST'] = 'f8ogy1hm9ubgfv2s.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
 
 	/**
 	 * user name
 	 */
-	$DB_ACCESS['DB_USER'] = 'root';
+	$DB_ACCESS['DB_USER'] = 'fwrwpolumasf0ilb';
 
 	/**
 	 * passoword if any
 	 */
-	$DB_ACCESS['DB_PASS'] = '';
+	$DB_ACCESS['DB_PASS'] = 'd7ilnzckkj45v82n';
 
 	/**
 	 * database name
 	 */
-	$DB_ACCESS['DB_NAME'] = 'p_l_db';
+	$DB_ACCESS['DB_NAME'] = 'efqr54vwc4uxerpy';
 
 ?>
