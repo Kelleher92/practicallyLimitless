15b91f12845b353bd90625b11e45476cd2710b67
Kelleher92 <iankelleher92@gmail.com>
2018-07-16 15:25:05 +0100

    Update db_cred.inc.php

--- sys/config/db_cred.inc.php
+++ sys/config/db_cred.inc.php
@@ -5,21 +5,25 @@
 	/**
 	 * server name
 	 */
-	$DB_ACCESS['DB_HOST'] = 'f8ogy1hm9ubgfv2s.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+	//$DB_ACCESS['DB_HOST'] = 'f8ogy1hm9ubgfv2s.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+	$DB_ACCESS['DB_HOST'] = getenv('DB_HOST');
 
 	/**
 	 * user name
 	 */
-	$DB_ACCESS['DB_USER'] = 'fwrwpolumasf0ilb';
+	//$DB_ACCESS['DB_USER'] = 'fwrwpolumasf0ilb';
+	$DB_ACCESS['DB_USER'] = getenv('DB_USER');
 
 	/**
 	 * passoword if any
 	 */
-	$DB_ACCESS['DB_PASS'] = 'd7ilnzckkj45v82n';
+	//$DB_ACCESS['DB_PASS'] = 'd7ilnzckkj45v82n';
+	$DB_ACCESS['DB_PASS'] = getenv('DB_PASS');
 
 	/**
 	 * database name
 	 */
-	$DB_ACCESS['DB_NAME'] = 'efqr54vwc4uxerpy';
+	//$DB_ACCESS['DB_NAME'] = 'efqr54vwc4uxerpy';
+	$DB_ACCESS['DB_NAME'] = getenv('DB_NAME');	
 
 ?>
