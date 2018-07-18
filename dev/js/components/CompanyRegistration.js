f4994f562c2e51494cd2b7bd93d5aa13152db104
Ian Kelleher <ikelleher@deloitte.ie>
2018-07-18 11:54:14 +0100

    Rebuild

--- dev/js/components/CompanyRegistration.js
+++ dev/js/components/CompanyRegistration.js
@@ -1,15 +1,7 @@
-/*
- * @Author: Sean Bermingham
- * @Date: 2018-07-16  
- */
-
  import React, {Component} from 'react';
- import $ from 'jquery';
-
- 
 
  export default class CompanyRegistration extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
             CompanyName: '',
@@ -18,50 +10,44 @@
             CompanyPassword: '',
             CompanyPasswordCheck: ''
         };
-        //anywhere that needs this.props
+
         this.onSubmit = this.onSubmit.bind(this);
     }
-    handleChange(name, e){
-        this.setState({ [name]: e.target.value });
+
+    handleChange(name, e) {
+        this.setState({[name]: e.target.value});
     }
-    isValidEmail(){
+
+    isValidEmail() {
         var emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        
         return emailPattern.test(this.state.CompanyEmail);
     }
-    isValidPassword(){
+
+    isValidPassword() {
         var passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
+        
         return passwordPattern.test(this.state.CompanyPassword);
     }
-    passwordRepeatedCorrectly(){
+
+    passwordRepeatedCorrectly() {
         return this.state.CompanyPassword === this.state.CompanyPasswordCheck;
     }
-    containsValue(str){
+
+    containsValue(str) {
         return !(str === "");
     }
+
     formIsSubmitable(){
 
     }
-    onSubmit(){
+
+    onSubmit() {
         console.log('Got into Submit ');
         console.log('The email is valid -- ' + this.isValidEmail(this.state.CompanyEmail));
         console.log('The password is valid -- ' +this.isValidPassword(this.state.CompanyPassword));
         console.log('The passwords are the same -- ' +this.passwordRepeatedCorrectly(this.state.CompanyPassword));
-         console.log('The value isnt blank -- ' +this.containsValue(this.state.CompanyName));
-        // $.ajax({
-        //     method: 'POST',
-        //     data: {
-        //         token: this.props.token,
-        //         action: 'registerCompany',
-        //         data: JSON.stringify({name: this.state.CompanyName, email: this.state.CompanyName, address: this.state.CompanyAddress, password: this.state.CompanyPassword})
-        //     },
-        //     url: 'public/process.php',
-        //     success: function(res) {
-        //         console.log(res);
-        //     },
-        //     error: function(res) {
-        //         console.log(res);
-        //     }
-        // });
+        console.log('The value isnt blank -- ' +this.containsValue(this.state.CompanyName));
     }
 
     render() {
@@ -72,25 +58,25 @@
                     <div className="form-input__heading">
                         Company Details
                     </div>
-                     <div className="form-input__section">
-                         <input id="CompanyName" type="text" name="CompanyName" placeholder="Company Name" className="form-input__value" onChange={(e) => this.handleChange("CompanyName", e)}/>
-                     </div>
-                      <div className="form-input__section">
-                         <input id="CompanyAddress" type="text" name="CompanyAddress" placeholder = "Company Address" className="form-input__value" onChange={(e) => this.handleChange("CompanyAddress", e)}/>
-                     </div>
                     <div className="form-input__section">
-                         <input id="CompanyEmail" type="email" name="CompanyEmail" placeholder = "Email Address" className="form-input__value" onChange={(e) => this.handleChange("CompanyEmail", e)}/>
-                     </div>
-                       <div className="form-input__section">
-                         <input id="CompanyPassword" type="password" name="CompanyPassword" placeholder = "Password" className="form-input__value" onChange={(e) => this.handleChange("CompanyPassword", e)}/>
-                     </div>
-                     <div className="form-input__section">
-                         <input id="CompanyPasswordCheck" type="password" name="CompanyPasswordCheck" placeholder = "Password Verification" className="form-input__value" onChange={(e) => this.handleChange("CompanyPasswordCheck", e)}/>
-                     </div>
-                 </div>
+                        <input id="CompanyName" type="text" name="CompanyName" placeholder="Company Name" className="form-input__value" onChange={(e) => this.handleChange("CompanyName", e)}/>
+                    </div>
+                    <div className="form-input__section">
+                        <input id="CompanyAddress" type="text" name="CompanyAddress" placeholder="Company Address" className="form-input__value" onChange={(e) => this.handleChange("CompanyAddress", e)}/>
+                    </div>
+                    <div className="form-input__section">
+                        <input id="CompanyEmail" type="email" name="CompanyEmail" placeholder="Email Address" className="form-input__value" onChange={(e) => this.handleChange("CompanyEmail", e)}/>
+                    </div>
+                    <div className="form-input__section">
+                        <input id="CompanyPassword" type="password" name="CompanyPassword" placeholder="Password" className="form-input__value" onChange={(e) => this.handleChange("CompanyPassword", e)}/>
+                    </div>
+                    <div className="form-input__section">
+                        <input id="CompanyPasswordCheck" type="password" name="CompanyPasswordCheck" placeholder="Password Verification" className="form-input__value" onChange={(e) => this.handleChange("CompanyPasswordCheck", e)}/>
+                    </div>
+                </div>
                 <button className="form__submit-button" onClick={this.onSubmit} disabled="true">Submit Company Details</button>
                  
-             </div>
+            </div>
         );
     }
  }
--- dev/js/components/PreLoader.js
+++ dev/js/components/PreLoader.js
@@ -1,14 +1,7 @@
-/*
- * @Author: Thomas Moran 
- * @Date: 2018-07-11 16:22:59 
- * @Last Modified by: Thomas Moran
- * @Last Modified time: 2018-07-12 09:43:10
- */
-
 import React, { Component } from 'react';
 
 export default class PreLoader extends Component {
-	render(){
+	render() {
 		return(
 			<div style={{"height":"100%"}} className="d-flex justify-content-center align-items-center">
 				<div className="preloader-lane">
