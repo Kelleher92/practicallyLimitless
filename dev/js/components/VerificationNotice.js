2cbf7e970dce34764ddf87dcdd863a17345884ee
Thomas Moran <thmoran@deloitte.ie>
2018-07-18 15:51:56 +0100

    VerificationNotice is now more modular

---
+++ dev/js/components/VerificationNotice.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+
+export default class VerificationNotice extends Component {
+    render() {
+        let status = this.props.verificationStatus;
+        return(
+            <div className={"verify-email__container " + (status ? "success": "fail")}>
+                <div className={"verify-email__icon " + (status ? "success": "fail")}>
+                    <i className={"far " + (status ? "fa-thumbs-up" : "fa-thumbs-down")}></i>
+                </div>
+                <div className="verify-email__status">{this.props.title}</div>
+
+                <div className="verify-email__sub-status">{this.props.subTitle}</div>
+                <div className="verify-email__button">
+                    <Link to={this.props.linkLocation}>
+                        <div className="pl-button--style-1">{this.props.linkText}</div>
+                    </Link>
+                </div>
+            </div>
+        );
+    }
+}
