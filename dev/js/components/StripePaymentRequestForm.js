4a54f5728dc50af7bf50da20a1159685dca31006
Ian Kelleher <iankelleher92@gmail.com>
2018-07-31 13:54:08 +0100

    Update StripePaymentRequestForm.js

--- dev/js/components/StripePaymentRequestForm.js
+++ dev/js/components/StripePaymentRequestForm.js
@@ -3,20 +3,23 @@ import {PaymentRequestButtonElement, injectStripe} from 'react-stripe-elements';
 
 const handleBlur = () => {
     console.log('[blur]');
-};
+}
+
 const handleChange = (change) => {
     console.log('[change]', change);
-};
+}
+
 const handleClick = () => {
     console.log('[click]');
-};
+}
+
 const handleFocus = () => {
     console.log('[focus]');
-};
+}
+
 const handleReady = () => {
     console.log('[ready]');
-};
-
+}
 
 class StripePaymentRequestForm extends Component {
     constructor(props) {
@@ -66,4 +69,5 @@ class StripePaymentRequestForm extends Component {
         ) : null;
     }
 }
+
 export default injectStripe(StripePaymentRequestForm);
