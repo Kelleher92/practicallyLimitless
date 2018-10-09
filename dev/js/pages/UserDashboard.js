5bec876e445a35e76ee2c0b95ec3e5284a8201a1
Ian Kelleher <iankelleher92@gmail.com>
2018-10-09 19:57:40 +0100

    Update UserDashboard.js

--- dev/js/pages/UserDashboard.js
+++ dev/js/pages/UserDashboard.js
@@ -8,11 +8,12 @@ import Footer from '../components/Footer';
 import UserDashboardTable from '../components/UserDashboardTable';
 import UserDashboardDetails from '../components/UserDashboardDetails';
 import AccountDashboardDetails from '../components/AccountDashboardDetails';
+
 class UserDashboard extends Component {
     constructor(props) {
         super(props);
         
-    this.state = {
+        this.state = {
             checkComplete: false,
             name: '',
             email: '',
@@ -27,16 +28,19 @@ class UserDashboard extends Component {
             tab: 0,
             newOffer: false
         }
+        
         this.onClickNew = this.onClickNew.bind(this);
         this.onClickUpdate = this.onClickUpdate.bind(this);
-    this.switchTab = this.switchTab.bind(this);
+        this.switchTab = this.switchTab.bind(this);
         this.updateDetails = this.updateDetails.bind(this);
         this.updateGeoCoor = this.updateGeoCoor.bind(this);
         this.updateSkills = this.updateSkills.bind(this);
         this.handleUpdateLogo = this.handleUpdateLogo.bind(this);
     }
+    
     componentDidMount() {
-    var me = this;
+        var me = this;
+        
         $.ajax({
             method: 'POST',
             data: {
@@ -80,15 +84,19 @@ class UserDashboard extends Component {
     onClickNew() {
         this.setState({newOffer: true});
     }
+    
     updateDetails(name, value) {
         this.setState({[name]: value});
     }
+    
     updateGeoCoor(newCoor) {
         this.setState({geoCoor: newCoor});
     }
+    
     updateSkills(newSkills) {
         this.setState({skills: newSkills});
     }
+    
     handleChange(name, e) {
         this.setState({[name]: e.target.value});
     }
@@ -96,9 +104,11 @@ class UserDashboard extends Component {
     switchTab(index) {
         this.setState({tab: index, newOffer: false});
     }
+    
     isSubmitable() {
         return isValidString(this.state.name)  && isValidString(this.state.geoCoor);
     }
+    
     onClickUpdate() {
         if(this.isSubmitable()) {
             let me = this;
@@ -133,9 +143,11 @@ class UserDashboard extends Component {
             this.props.showFlashNotification('Update of information failed, please try again.');
         }
     }
+    
     handleUpdateLogo(newLogo) {
         this.setState({logo:newLogo});
     }
+    
     render() {
         return (
             <div className="contain">               
@@ -189,4 +201,5 @@ class UserDashboard extends Component {
         );
     }
 }
+
 export default withRouter(UserDashboard);
