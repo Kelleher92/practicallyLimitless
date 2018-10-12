1c3bc4ebb7309b90c2298648694a3b195a6b8a15
Ian Kelleher <iankelleher92@gmail.com>
2018-10-12 15:04:37 +0100

    Update Cards.js

--- dev/js/components/Cards.js
+++ dev/js/components/Cards.js
@@ -1,27 +1,21 @@
 import React, {Component} from 'react';
 
-
 class Cards extends Component {
    render() {
 
     let status = this.props.cardStatus;
        return (
-           
            <div className=".col-6 .col-sm-4">
-           <div className="form__cardcontainer">
+               <div className="form__cardcontainer">
                    <div><img src={this.props.logo} alt="logo" className="form-cardlogo"/> </div>
                    <div className="form-cardheader"> {this.props.name} </div>
                    <div className="form-cardoffername"> {this.props.description} </div>
                    <div className="form-cardoffername"> {this.props.date} </div>
                    <button className="pl-button--style-2" >Apply</button>
-                   </div>
-                   </div>
-    
-
+                </div>
+           </div>
        );
    }
 }
 
 export default Cards;
-
-       
