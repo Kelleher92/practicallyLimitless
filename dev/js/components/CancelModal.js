5cf3a6b08ed2c796ae7c203f62960e42a84f544b
Staab <pstaab@deloitte.ie>
2018-10-12 06:19:31 +0100

    Adding a Cancel Delete Modal

---
+++ dev/js/components/CancelModal.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class CancelModal extends React.Component {
+  render() {
+    if(!this.props.show) {
+      return null;
+    }
+
+    return (
+             <div className="modal" >
+                <div className="modal-dialog modal-lg modal-dialog-centered">
+                    <div className="modal-content">
+                        {this.props.children}
+                        <div>
+                        <button className="pl-button--style-2" onClick={this.props.onClose}>Cancel</button>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+    );
+
+  }
+}
+
+CancelModal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  show: PropTypes.bool,
+  children: PropTypes.node
+};
+
+export default CancelModal;
